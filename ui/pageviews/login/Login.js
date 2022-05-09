55b58166c1d5b1a1fa508bc65d4588543a61a131
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-09 19:08:50 +0200

    add login form component

--- ui/pageviews/login/Login.js
+++ ui/pageviews/login/Login.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import LoginForm from "./loginForm/LoginForm";
+
 function Login() {
 	return (
 		<>
 			<div>Login</div>
+			<LoginForm />
 		</>
 	);
 }
---
+++ ui/pageviews/login/loginForm/LoginForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import cl from "./LoginForm.module.scss";
+
+function LoginForm() {
+	return (
+		<>
+			<div className={cl.header}>LoginForm</div>
+			<p>this is the login fomre</p>
+		</>
+	);
+}
+
+export default LoginForm;
