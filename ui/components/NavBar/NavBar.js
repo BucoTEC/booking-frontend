5a6d017115b8a65b11c8428eed4ed518f74583a5
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-09 18:52:45 +0200

    add basic links to navbar

--- ui/components/NavBar/NavBar.js
+++ ui/components/NavBar/NavBar.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Link from "next/link";
 
 import cl from "./NavBar.module.scss";
 
 function NavBar() {
-	return <div className={cl.wrapper}>NavBar</div>;
+	return (
+		<div className={cl.wrapper}>
+			<Link href="/">test1</Link>
+			<Link href="/">test2</Link>
+			<Link href="/">test3</Link>
+			<Link href="/">test4</Link>
+			<Link href="/">test5</Link>
+			<Link href="/">test6</Link>
+		</div>
+	);
 }
 
 export default NavBar;
