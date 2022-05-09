96318417e15e0648e0a54151d3a63007de40f894
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-09 23:08:00 +0200

    implemetn collecting data from login form

--- ui/pageviews/login/loginForm/LoginForm.js
+++ ui/pageviews/login/loginForm/LoginForm.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 import cl from "./LoginForm.module.scss";
 
 function LoginForm() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const emailHandler = (e) => {
+		setEmail(e.target.value);
+	};
+
+	const passwordHandler = (e) => {
+		setPassword(e.target.value);
+	};
+
+	const submitHandler = () => {
+		console.log(email, password);
+		setEmail("");
+		setPassword("");
+	};
 	return (
 		<div className={cl.input_wrapper}>
 			<h1 className={cl.header}>Login</h1>
 
-			<input type="email" name="email" placeholder="email" />
-			<input type="password" name="password" placeholder="password" />
-			<button>Login</button>
+			<input
+				type="email"
+				name="email"
+				placeholder="email"
+				value={email}
+				onChange={emailHandler}
+				required
+				min="3"
+			/>
+			<input
+				type="password"
+				name="password"
+				placeholder="password"
+				value={password}
+				onChange={passwordHandler}
+				required
+				min="3"
+			/>
+			<button onClick={submitHandler}>Login</button>
 		</div>
 	);
 }
