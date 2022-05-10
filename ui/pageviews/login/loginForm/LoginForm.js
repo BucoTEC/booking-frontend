1b1d46663f858b97e23f7b60d80447f8ea53ebc0
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-11 00:38:51 +0200

    add current user to context on login

--- ui/pageviews/login/loginForm/LoginForm.js
+++ ui/pageviews/login/loginForm/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { UserContext } from "../../../../context/UserContext";
 
 import cl from "./LoginForm.module.scss";
@@ -6,14 +6,15 @@ import cl from "./LoginForm.module.scss";
 import instance from "../../../../api/axiosInstance";
 import axios from "axios";
 
-//TODO add user to context when login
 // TODO fix axios instance on request
 
 function LoginForm() {
 	const { currentUser, setCurrentUser } = useContext(UserContext);
 	const [email, setEmail] = useState("");
+	const [isLoading, setIsLoadin] = useState(false);
+	// const [err, setErr] = useState(null);
+
 	const [password, setPassword] = useState("");
-	console.log(currentUser);
 	const emailHandler = (e) => {
 		setEmail(e.target.value);
 	};
@@ -24,41 +25,50 @@ function LoginForm() {
 
 	const submitHandler = async () => {
 		try {
+			setIsLoadin(true);
 			const res = await axios.post("http://localhost:5000/api/auth/login", {
 				email,
 				password,
 			});
-			console.log(res.data);
+			setCurrentUser(res.data);
+			setIsLoadin(false);
 		} catch (error) {
+			setIsLoadin(false);
 			console.log(error);
 		}
 
 		// setEmail("");
 		// setPassword("");
 	};
+
 	return (
 		<div className={cl.input_wrapper}>
 			<h1 className={cl.header}>Login</h1>
-
-			<input
-				type="email"
-				name="email"
-				placeholder="email"
-				value={email}
-				onChange={emailHandler}
-				required
-				min="3"
-			/>
-			<input
-				type="password"
-				name="password"
-				placeholder="password"
-				value={password}
-				onChange={passwordHandler}
-				required
-				min="3"
-			/>
-			<button onClick={submitHandler}>Login</button>
+			{isLoading ? (
+				<h1>Loading ...</h1>
+			) : (
+				<>
+					<input
+						type="email"
+						name="email"
+						placeholder="email"
+						value={email}
+						onChange={emailHandler}
+						required
+						min="3"
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="password"
+						value={password}
+						onChange={passwordHandler}
+						required
+						min="3"
+					/>
+					<button onClick={submitHandler}>Login</button>
+				</>
+			)}
 		</div>
 	);
 }
