c2cc0b3cf675a437faf99d0729f0c66bceb25e11
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-10 22:44:02 +0200

    add basic user context

---
+++ context/UserContext.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { createContext, useEffect, useReducer } from "react";
+
+const INITIAL_STATE = {
+	user: JSON.parse(localStorage.getItem("booking_user")) || null,
+};
+
+export const UserContext = createContext(INITIAL_STATE);
+
+export const UserContextProvider = ({ children }) => {
+	const [currentUser, setCurrentUser] = useState(INITIAL_STATE);
+
+	useEffect(() => {
+		localStorage.setItem("booking_user", JSON.stringify(currentUser));
+	}, [currentUser]);
+	return (
+		<>
+			<UserContext.Provider value={{ currentUser, setCurrentUser }}>
+				{children}
+			</UserContext.Provider>
+		</>
+	);
+};
+
+export default UserContext;
