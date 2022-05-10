bb29e25069124530773f494067a2b82851029642
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-10 22:46:04 +0200

    add context provider to main

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,17 @@ import "../styles/globals.css";
 import NavBar from "../ui/components/NavBar/NavBar";
 import MainLayout from "../ui/components/mainLayout/MainLayout";
 
+import { UserContextProvider } from "../context/UserContext.js";
+
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<NavBar />
-			<MainLayout>
-				<Component {...pageProps} />
-			</MainLayout>
+			<UserContextProvider>
+				<MainLayout>
+					<Component {...pageProps} />
+				</MainLayout>
+			</UserContextProvider>
 		</>
 	);
 }
