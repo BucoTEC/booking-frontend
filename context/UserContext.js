a1310cb861c705aa5f79492cfc48c1e78ea5f9e8
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-13 23:49:24 +0200

    returne intial state

--- context/UserContext.js
+++ context/UserContext.js
@@ -4,17 +4,16 @@ import { createContext, useEffect, useReducer } from "react";
 // TODO check how to get local storege in a next js app
 
 const INITIAL_STATE = {
-	user: "",
+	user: localStorage.getItem("user"),
 };
 
 export const UserContext = createContext(INITIAL_STATE);
 
 export const UserContextProvider = ({ children }) => {
 	const [currentUser, setCurrentUser] = useState(INITIAL_STATE);
-
 	useEffect(() => {
 		console.log(currentUser);
-		localStorage.setItem("user", JSON.stringify(currentUser.userId));
+		localStorage.setItem("user", JSON.stringify(currentUser?.userId));
 	}, [currentUser]);
 	return (
 		<>
@@ -24,3 +23,10 @@ export const UserContextProvider = ({ children }) => {
 		</>
 	);
 };
+
+export async function getStaticProps() {
+	// const user = localStorage.getItem("user");
+	return {
+		props: { user }, // will be passed to the page component as props
+	};
+}
