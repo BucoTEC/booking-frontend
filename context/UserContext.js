c9d82608b3ef8148fd635820af0c2329dbbef8e3
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-15 23:18:14 +0200

    fix context handler user refresh

--- context/UserContext.js
+++ context/UserContext.js
@@ -1,22 +1,19 @@
 import React, { useState, useEffect, createContext } from "react";
 
-// TODO check how to get local storege in a next js app
-
 export const UserContext = createContext();
 
 export const UserContextProvider = ({ children }) => {
 	const [currentUser, setCurrentUser] = useState("");
+
+	// initialize curretn user state
 	useEffect(() => {
-		try {
-			const localUser = JSON.parse(localStorage.getItem("current_user"));
-			if (localUser) {
-				setCurrentUser(JSON.parse(localStorage.getItem("current_user")));
-			}
-		} catch (error) {
-			console.log(error);
+		const localUser = JSON.parse(localStorage.getItem("current_user"));
+		if (localUser) {
+			setCurrentUser(JSON.parse(localStorage.getItem("current_user")));
 		}
 	}, []);
 
+	//check for changes in current user state
 	useEffect(() => {
 		if (currentUser.userId) {
 			localStorage.setItem("current_user", JSON.stringify(currentUser.userId));
