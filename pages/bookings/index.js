813b50e736f192a390f79d2a7e14226ac13fb0be
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-17 23:51:01 +0200

    add create a booking button

--- pages/bookings/index.js
+++ pages/bookings/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import Link from "next/link";
+
 function Bookings() {
-	return <div>Bookings</div>;
+	return (
+		<div>
+			<h1>this is the bookings page</h1>
+			<button style={{ padding: "0.5rem 1rem" }}>
+				<Link href="/bookings/create">Create a booking</Link>
+			</button>
+		</div>
+	);
 }
 
 export default Bookings;
