6afeacde1fce2eeb2349448a2732721aa0fbc02e
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-17 23:30:40 +0200

    add expiry date to user

--- context/UserContext.js
+++ context/UserContext.js
@@ -12,13 +12,27 @@ export const UserContextProvider = ({ children }) => {
 
 	// initialize curretn user state
 	useEffect(() => {
-		setCurrentUser(JSON.parse(localStorage.getItem("current_user")));
+		const data = JSON.parse(localStorage.getItem("current_user"));
+		const now = new Date();
+		if (data && now.getTime() < data.expiry) {
+			setCurrentUser(data.currentUser);
+		} else {
+			localStorage.removeItem("current_user");
+			setCurrentUser(null);
+		}
 	}, []);
 
 	//check for changes in current user state
 	useEffect(() => {
 		if (currentUser) {
-			localStorage.setItem("current_user", JSON.stringify(currentUser));
+			const time = new Date();
+			localStorage.setItem(
+				"current_user",
+				JSON.stringify({
+					currentUser,
+					expiry: time + 172800000, //   2 days in miliseconds
+				})
+			);
 		}
 	}, [currentUser?.userId, currentUser, setCurrentUser]);
 
