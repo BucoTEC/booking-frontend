f4d8e3a511b895f9e48e77a29d36097a72ab5aea
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-18 00:00:52 +0200

    fix heading on bookings page

--- pages/bookings/index.js
+++ pages/bookings/index.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 function Bookings() {
 	return (
 		<div>
-			<h1>this is the bookings page</h1>
+			<h1>This is the bookings page</h1>
 			<button style={{ padding: "0.5rem 1rem" }}>
 				<Link href="/bookings/create">Create a booking</Link>
 			</button>
