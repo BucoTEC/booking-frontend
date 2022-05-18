a4d19f9cfc6e6d1f69f0292d54f2db4b3e588d06
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-18 19:02:02 +0200

    setup simple date picker

--- pages/bookings/create.js
+++ pages/bookings/create.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function CreateBooking() {
-	return <div>CreateBooking</div>;
+	const [startDate, setStartDate] = useState(new Date());
+	let handleColor = (time) => {
+		return time.getHours() > 12 ? "text-success" : "text-error";
+	};
+	return (
+		<>
+			<h1>Add a booking</h1>
+			<div>
+				<DatePicker
+					showTimeSelect
+					selected={startDate}
+					onChange={(date) => setStartDate(date)}
+					timeClassName={handleColor}
+				/>
+			</div>
+		</>
+	);
 }
 
 export default CreateBooking;
