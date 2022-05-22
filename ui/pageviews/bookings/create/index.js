413928922ec302aad1b5536949b5b5cc14ed6e37
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-22 21:36:27 +0200

    add prevent default handler

--- ui/pageviews/bookings/create/index.js
+++ ui/pageviews/bookings/create/index.js
@@ -14,33 +14,40 @@ function CreateBookingPageView() {
 		const day = getDay(date);
 		return day !== 0;
 	};
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+	};
 	return (
-		<div>
-			<DatePicker
-				required
-				showTimeSelect
-				inline
-				minDate={now}
-				maxDate={addDays(now, +14)}
-				timeFormat="HH:mm"
-				minTime={setHours(setMinutes(now, 0), 9)}
-				maxTime={setHours(setMinutes(now, 30), 18)}
-				filterDate={isWeekday}
-				selected={startDate}
-				onChange={(date) => setStartDate(date)}
-			/>
-			<div className={cl.omotac}>
-				<textarea rows="15" cols="37" required></textarea>
-				<select name="num" id="num" required>
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-					<option value="5">5</option>
-					<option value="6">6</option>
-				</select>
-			</div>
-		</div>
+		<>
+			<form action="/" onSubmit={(e) => submitHandler(e)}>
+				<DatePicker
+					required
+					showTimeSelect
+					inline
+					minDate={now}
+					maxDate={addDays(now, +14)}
+					timeFormat="HH:mm"
+					minTime={setHours(setMinutes(now, 0), 9)}
+					maxTime={setHours(setMinutes(now, 30), 18)}
+					filterDate={isWeekday}
+					selected={startDate}
+					onChange={(date) => setStartDate(date)}
+				/>
+				<div className={cl.omotac}>
+					<textarea rows="15" cols="37" required></textarea>
+					<select name="num" id="num" required>
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+						<option value="6">6</option>
+					</select>
+				</div>
+				<button type="submit">make new booking</button>
+			</form>
+		</>
 	);
 }
 
