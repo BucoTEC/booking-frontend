f7e914fa0d3c15f5842b82bc9e2f3c4a55554b91
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-22 21:29:46 +0200

    fix alignmet

--- ui/pageviews/bookings/create/index.js
+++ ui/pageviews/bookings/create/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import cl from "./createBookingPageView.module.scss";
 import "react-datepicker/dist/react-datepicker.css";
@@ -9,9 +9,6 @@ import { setHours, setMinutes, addDays, getDay } from "date-fns";
 function CreateBookingPageView() {
 	const now = new Date();
 	const [startDate, setStartDate] = useState(now);
-	useEffect(() => {
-		console.log(startDate);
-	}, [startDate]);
 
 	const isWeekday = (date) => {
 		const day = getDay(date);
@@ -19,23 +16,22 @@ function CreateBookingPageView() {
 	};
 	return (
 		<div>
+			<DatePicker
+				required
+				showTimeSelect
+				inline
+				minDate={now}
+				maxDate={addDays(now, +14)}
+				timeFormat="HH:mm"
+				minTime={setHours(setMinutes(now, 0), 9)}
+				maxTime={setHours(setMinutes(now, 30), 18)}
+				filterDate={isWeekday}
+				selected={startDate}
+				onChange={(date) => setStartDate(date)}
+			/>
 			<div className={cl.omotac}>
-				<div className={cl.pic}>
-					<DatePicker
-						showTimeSelect
-						inline
-						minDate={now}
-						maxDate={addDays(now, +14)}
-						timeFormat="HH:mm"
-						minTime={setHours(setMinutes(now, 0), 9)}
-						maxTime={setHours(setMinutes(now, 30), 18)}
-						filterDate={isWeekday}
-						selected={startDate}
-						onChange={(date) => setStartDate(date)}
-					/>
-				</div>
-				<textarea rows="15" cols="37"></textarea>
-				<select name="num" id="num">
+				<textarea rows="15" cols="37" required></textarea>
+				<select name="num" id="num" required>
 					<option value="1">1</option>
 					<option value="2">2</option>
 					<option value="3">3</option>
