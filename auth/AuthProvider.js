ec8d21b7b8fe39677abb31bd804f57fc1cf28713
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-25 00:35:40 +0200

    cleen up auth provider

--- auth/AuthProvider.js
+++ auth/AuthProvider.js
@@ -10,21 +10,6 @@ function AuthProvider({ pageProps, children }) {
 	const { currentUser } = useContext(UserContext);
 	const [loading, setLoading] = useState(true);
 
-	// auth checker
-	// useEffect(() => {
-	// 	if (!currentUser?.userId && pageProps?.auth) {
-	// 		return <h1>loading</h1>;
-	// 	} else if (pageProps?.admin && !currentUser?.isAdmin) {
-	// 		return <h1>loading</h1>;
-	// 	}
-	// }, [
-	// 	pageProps?.auth,
-	// 	pageProps?.admin,
-	// 	currentUser,
-	// 	currentUser?.isAdmin,
-	// 	router,
-	// 	pageProps,
-	// ]);
 	useEffect(() => {
 		setLoading(false);
 	}, []);
