8c198d7ac35f1f99c73d50f57d0735c28f815257
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-25 23:25:32 +0200

    add register form layout

--- ui/pageviews/register/registerForm/RegisterForm.js
+++ ui/pageviews/register/registerForm/RegisterForm.js
@@ -3,7 +3,36 @@ import cl from "./RegisterForm.module.scss";
 
 function RegisterForm() {
 	console.log("register form");
-	return <div>RegisterForm</div>;
+	return (
+		<div className={cl.input_wrapper}>
+			<h1 className={cl.header}>Register</h1>
+			{isLoading ? (
+				<h1>Loading ...</h1>
+			) : (
+				<>
+					<input
+						type="email"
+						name="email"
+						placeholder="email"
+						value={email}
+						onChange={emailHandler}
+						required
+						min="3"
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="password"
+						value={password}
+						onChange={passwordHandler}
+						required
+						min="3"
+					/>
+					<button onClick={submitHandler}>Login</button>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default RegisterForm;
