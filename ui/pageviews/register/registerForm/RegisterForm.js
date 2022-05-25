e4a3ddfbf081dacf668527856ff1b19a0b6015b5
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-25 23:24:04 +0200

    add register form component

---
+++ ui/pageviews/register/registerForm/RegisterForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+import cl from "./RegisterForm.module.scss";
+
+function RegisterForm() {
+	console.log("register form");
+	return <div>RegisterForm</div>;
+}
+
+export default RegisterForm;
