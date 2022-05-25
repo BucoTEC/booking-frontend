385325f2c3297075294826e9d038a98cf76583c6
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-25 23:29:49 +0200

    add register form to register page view

--- ui/pageviews/register/index.js
+++ ui/pageviews/register/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import RegisterForm from "./registerForm/RegisterForm";
 
 function Register() {
-	return <div>Register</div>;
+	return (
+		<>
+			<h1>Register page</h1>
+			<RegisterForm />
+		</>
+	);
 }
 
 export default Register;
