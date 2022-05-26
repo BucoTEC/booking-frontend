bc3d1c579d6db0b2c16474ad0bfc4ef938fd0a45
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-26 22:18:43 +0200

    modified basic booking component

--- ui/pageviews/bookings/allBookings/BookingList/Booking/index.js
+++ ui/pageviews/bookings/allBookings/BookingList/Booking/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import cl from "./Booking.module.scss";
+
 function Booking({ data }) {
-	return <div>{data?._id}</div>;
+	return (
+		<div className={cl.bookingWrapper}>
+			<div>
+				<h3>Date</h3>
+				{data.date}
+			</div>
+			{data?._id}
+		</div>
+	);
 }
 
 export default Booking;
