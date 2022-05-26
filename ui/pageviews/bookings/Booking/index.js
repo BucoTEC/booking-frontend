1e84890eb5a555b093ad433a4b10c4d12ec517f8
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-26 22:37:53 +0200

    add delete booking basic handler

--- ui/pageviews/bookings/Booking/index.js
+++ ui/pageviews/bookings/Booking/index.js
@@ -6,13 +6,17 @@ import cl from "./Booking.module.scss";
 function Booking() {
 	const router = useRouter();
 
+	const deleteHandler = () => {
+		console.log("delete booking");
+	};
+
 	return (
 		<div className={cl.bookingWrapper}>
 			<h1>single booking</h1>
 			<h3>{router.query.id}</h3>
 			<div>
 				<button>edit booking</button>
-				<button>delete booking</button>
+				<button onClick={deleteHandler}>delete booking</button>
 			</div>
 		</div>
 	);
