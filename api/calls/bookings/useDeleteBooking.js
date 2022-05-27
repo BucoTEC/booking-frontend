9b9e0441f9892e7400875f635b0f4dbc68f674df
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-27 23:41:56 +0200

    add basic logic to  useDeletbooking

--- api/calls/bookings/useDeleteBooking.js
+++ api/calls/bookings/useDeleteBooking.js
@@ -1,7 +1,51 @@
-import React from "react";
+import { useState, useContext } from "react";
 
 function useDeleteBooking() {
-	return <div>useDeleteBooking</div>;
+	const [isLoading, setIsLoading] = useState(false);
+	const { currentUser } = useContext(UserContext);
+
+	const deleteBooking = async () => {
+		try {
+			setIsLoading(true);
+			const res = await instance.post(
+				"/bookings",
+				{
+					date: startDate,
+					comment: textMessage,
+					amount: Number(numOfCustomers),
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${currentUser.token}`,
+					},
+				}
+			);
+			console.log(res);
+			setIsLoading(false);
+			toast.success("success", {
+				position: "bottom-center",
+				autoClose: 5000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+			});
+		} catch (err) {
+			setIsLoading(false);
+			console.log(err);
+			toast.error("fail", {
+				position: "bottom-center",
+				autoClose: 5000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+			});
+		}
+	};
+	return { deleteBooking, isLoading };
 }
 
 export default useDeleteBooking;
