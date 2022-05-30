540abb8a25899645d4b5392ce709dd2a51a747fe
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-30 22:45:33 +0200

    add inputs to updatebooking

--- ui/components/BookingRelated/UpdateBooking/index.js
+++ ui/components/BookingRelated/UpdateBooking/index.js
@@ -1,7 +1,45 @@
 import React from "react";
 
 function UpdateBooking() {
-	return <div>UpdateBooking</div>;
+	return (
+		<form action="/" onSubmit={(e) => submitHandler(e)}>
+			<DatePicker
+				required
+				showTimeSelect
+				inline
+				minDate={now}
+				maxDate={addDays(now, +14)}
+				timeFormat="HH:mm"
+				minTime={setHours(setMinutes(now, 0), 9)}
+				maxTime={setHours(setMinutes(now, 30), 18)}
+				filterDate={isWeekday}
+				selected={startDate}
+				onChange={(date) => setStartDate(date)}
+			/>
+			<div className={cl.omotac}>
+				<textarea
+					rows="15"
+					cols="37"
+					required
+					onChange={(e) => setTextMessage(e.target.value)}
+				></textarea>
+				<select
+					name="num"
+					id="num"
+					required
+					onChange={(e) => setNumOfCustomers(e.target.value)}
+				>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+					<option value="6">6</option>
+				</select>
+			</div>
+			<button type="submit">make new booking</button>
+		</form>
+	);
 }
 
 export default UpdateBooking;
