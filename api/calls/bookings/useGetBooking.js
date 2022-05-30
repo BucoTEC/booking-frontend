9ab348d9aed21cbe56d8422dcff4ddd55398254f
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-30 22:50:54 +0200

    add setup for useGetbooking

---
+++ api/calls/bookings/useGetBooking.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function useGetBooking() {
+	return <div>useGetBooking</div>;
+}
+
+export default useGetBooking;
--- ui/pageviews/bookings/Booking/index.js
+++ ui/pageviews/bookings/Booking/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
 import cl from "./Booking.module.scss";
