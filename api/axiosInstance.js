972796dbf94fed0f9fd560238f790c67f0b6cf1a
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-04 12:51:41 +0200

    remove time out period

--- api/axiosInstance.js
+++ api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
 	baseURL: "https://btech-solutions-booking-api.herokuapp.com/api",
-	timeout: 1000,
+	// timeout: 1000,
 });
 
 export default instance;
